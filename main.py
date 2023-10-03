3a9ea199c44e0f868637fc0c0e3549cf45022417
SuEsmerino <126846253+SuEsmerino@users.noreply.github.com>
2023-10-03 18:59:11 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,41 @@
+nome_do_aluno = "Susana" #Tipo string (str)
+idade = 31 #Tipo inteiro (int)
+altura = 1.60 #Tipo decial (float)
+matriculado = True #Tipo boleano (bool)
+
+type(idade)
+print("Hello World")
+
+
+print(type(nome_do_aluno))
+print(type(idade))
+print(type(altura))
+print(type(altura))
+print(type(altura))
+print(type(matriculado))
+
+numero1 = 8
+numero2 = 5
+
+print(numero1 + numero2)
+print(numero1 * numero2)
+print(numero1 / numero2)
+print(numero1 ** numero2)
+print(numero1 % 2)
+print(numero2 % 2)
+
+
+n1 = 9
+n2 = 4
+
+print(n1 == n2) 
+print(n1 != n2)
+print(n1 > n2)
+print(n1 < n2)
+print(n1 >= n2)
+print(n1 <= n2)
+
+
+nome_do_aluno = input("Digite o seu nome: ")
+print("Bem vindo (a)", nome_do_aluno,"!") 
+print(f"Bem vindo {nome_do_aluno}!")
---
+++ questoes.py
@@ -0,0 +1,76 @@
+# # #Questão 01 
+
+# print("Olá Mundo!")
+
+# # #Questão 02 
+
+# nome = str(input("Digite seu nome:"))
+# print(f"Olá, {nome}!")
+
+# # Questão 03
+
+# idade = int(input("Digite sua idade:"))
+# print(f"Sua idade é: {idade} anos")
+
+#Questao 04
+
+informacoes001 = str(input("Digite seu nome:"))
+informacoes002 = int(input("Digite sua idade:"))
+print(f"""Seu nome é {informacoes001})
+      e você tem {informacoes002} anos.""")
+
+
+#Questao 05
+
+informacoes = str(input("Digite seu nome:"))
+informacoes02 = int(input("Digite sua idade:"))
+print(f"Seu nome é {informacoes} e você tem {informacoes02} anos.")
+
+#Questao 06
+
+numero1 = int(input("Digite um número: "))
+numero2 = int(input("Digite outro número: "))
+
+
+print(f"A soma dos números digitados é{numero1 + numero2}")
+
+
+#Questao 07
+
+
+numero003 = int(input("Digite um número: "))
+numero004 = int(input("Digite outro número: "))
+
+print(f"A subtração dos números digitados é {numero003 - numero004}")
+
+#Questao 08
+
+numero005 = int(input("Digite um número: "))
+numero006 = int(input("Digite outro número: "))
+
+print(f"A multiplicação dos números digitados é {numero005 * numero006}")
+
+
+#Questao 09
+
+numero007 = int(input("Digite um número: "))
+numero008 = int(input("Digite outro número: "))
+
+print(f"A divisão dos números digitados é {numero007 / numero008}")
+
+
+#Questao 10
+
+numero009 = int(input("Digite um número: "))
+numero010 = int(input("Digite outro número: "))
+
+print(f"O resto da divisão dos números digitados é {numero009 % numero010}")
+
+#Questao 11
+
+numero011 = int(input("Digite um número: "))
+numero012 = int(input("Digite outro número: "))
+
+print(f"A divisão dos números digitados é {numero007 / numero008}")
+
+
