bd0ad5f8a8dbaf357740d26c74e6faefb1e84bbf
SuEsmerino <126846253+SuEsmerino@users.noreply.github.com>
2023-10-10 16:44:10 -0300

    Add files via upload

---
+++ aula02.logica/semaforo.py
@@ -0,0 +1,10 @@
+cor = str(input("Escolha uma cor: verde vermelho ou amarelo: "))
+
+if cor == "verde":
+    print("O sinal está verde, pode acelerar! ")
+
+elif cor == "vermelho":
+    print("Vish, sinal vermelho, PARE!")
+
+else:
+    print("Atenção, o sinal está amarelo!")
